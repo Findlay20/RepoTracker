37c08356bdbc168c24e6371c6b365a6dff3e9c04
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-29 15:17:28 +0000

    Search box works but doesn't call anything on submit yet

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Search () {
     
     const [searchValue, setSearchValue] = useState()
 
-
+    const handleSubmit = e => {
+        e.preventDefault()
+        // getResult(searchValue);
+        document.getElementById('searchBox').value = "";
+    }
     
+    const updateInput = e => {
+        const input = e.target.value 
+        setSearchValue(input)
+    }
+
     return <div id="search">
         <h1>Search Section</h1>
         <form onSubmit={handleSubmit}>
-            <input type="text" id="searchBox" required placeholder='Enter Github name...'/>
+            <input type="text" id="searchBox" required placeholder='Enter Github name...' onChange={updateInput}/>
             <input type="submit" value="Search"/>
         </form>
     </div>
