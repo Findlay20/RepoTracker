cfd4c819395c4ea22a13bc558817aafa615a671a
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-11-29 14:39:18 +0000

    General structure to page and minimal styling

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header, Search } from './components';
+
+function App() {
+    return (
+        <div id="app">
+            <Header />
+            <main>
+                <Search />
+            </main>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Search () {
+    
+    return <h1>Search area</h1>
+}
+
+export default Search;
